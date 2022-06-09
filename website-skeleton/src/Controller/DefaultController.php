508d6eb632578f2d5313819fb2468fc4f09b6fef
Nazim Boudeffa <boudeffa@gmail.com>
2022-06-09 15:12:36 +0200

    First Commit

---
+++ website-skeleton/src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+
+    public function test()
+    {
+        var_dump("it works!");
+        die();
+    }
+}
